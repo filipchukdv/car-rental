61ef559af45cdaa17a876eafb2125a99bca4db50
filipchukdv <filipchukdv@gmail.com>
2023-09-17 23:55:46 +0300

    deployment fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="car-rental">
+    <BrowserRouter basename="/car-rental">
       <App />
     </BrowserRouter>
   </React.StrictMode>
