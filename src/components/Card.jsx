e1993c8d073a316b6242f312d55b067ae9a96b49
filipchukdv <filipchukdv@gmail.com>
2023-09-17 20:50:16 +0300

    added Card mark up

---
+++ src/components/Card.jsx
@@ -0,0 +1,64 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+
+const Card = ({ data }) => {
+  const address = data.address.split(",");
+  return (
+    <Box
+      sx={{
+        width: 274,
+        border: "1px solid black",
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        width={274}
+        height={268}
+        sx={{ borderRadius: "14px", overflow: "hidden" }}
+        mb="14px"
+      >
+        <img
+          style={{
+            display: "block",
+            height: "100%",
+            width: "100%",
+            objectFit: "cover",
+          }}
+          src={data.img}
+          alt={`${data.make} ${data.model}`}
+        />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }} mb="8px">
+        <Typography>
+          {data.make}{" "}
+          <span style={{ color: "rgba(52, 112, 255, 1)" }}>{data.model}</span>,
+          {data.year}
+        </Typography>
+        <Typography pr="9px">{data.rentalPrice}</Typography>
+      </Box>
+      <Typography sx={{ fontSize: "12px", color: "rgba(18, 20, 23, 0.5)" }}>
+        {address[1]} | {address[2]} | {data.rentalCompany} | {data.type}
+      </Typography>
+      <Typography
+        sx={{ fontSize: "12px", color: "rgba(18, 20, 23, 0.5)" }}
+        mb="28px"
+      >
+        {data.mileage} | {data.accessories[0]} | {data.accessories[1]}
+      </Typography>
+      <Button
+        sx={{
+          width: "100%",
+          bgcolor: "rgba(52, 112, 255, 1)",
+          color: "rgba(255, 255, 255, 1)",
+          textTransform: "capitalize",
+          "&:hover": { bgcolor: " rgba(11, 68, 205, 1) " },
+          padding: "12px",
+        }}
+      >
+        Learn more
+      </Button>
+    </Box>
+  );
+};
+
+export default Card;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,57 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
+import { Grid } from "@mui/material";
 
 const Catalog = () => {
+  const cardExample = {
+    id: 19590,
+    year: 2018,
+    make: "Subaru",
+    model: "Outback",
+    type: "SUV",
+    img: "https://focus.ua/static/storage/thumbs/1200x630/3/87/c2ef89af-71cee569e75f6065eb5ab256db984873.jpeg?v=9971_1",
+    description:
+      "The Subaru Outback is a versatile and reliable SUV that combines off-road capability with a comfortable and spacious interior.",
+    fuelConsumption: "8.7",
+    engineSize: "2.5L 4-cylinder",
+    accessories: [
+      "Leather upholstery",
+      "Power moonroof",
+      "Harman Kardon premium audio system",
+    ],
+    functionalities: [
+      "Symmetrical All-Wheel Drive",
+      "X-Mode off-road assist",
+      "Subaru EyeSight driver-assist system",
+    ],
+    rentalPrice: "$40",
+    rentalCompany: "Adventure Car Rentals",
+    address: "987 Example Street, Kyiv, Ukraine",
+    rentalConditions:
+      "Minimum age: 21\nValid driver's license\nCredit card required",
+    mileage: 1061,
+  };
+
   return (
     <>
       <SearchBar />
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        columnSpacing="29px"
+      >
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+        <Card data={cardExample} />
+      </Grid>
     </>
   );
 };
